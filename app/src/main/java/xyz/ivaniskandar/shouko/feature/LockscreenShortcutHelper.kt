c99bae362a2ec74711977bd1e6c05784f2cacab2
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-03-22 01:14:21 +0700

    LockscreenShortcutHelper: Only register the receiver when custom shortcut is used

--- app/src/main/java/xyz/ivaniskandar/shouko/feature/LockscreenShortcutHelper.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/feature/LockscreenShortcutHelper.kt
@@ -29,7 +29,8 @@ import xyz.ivaniskandar.shouko.util.canWriteSecureSettings
 class LockscreenShortcutHelper(
     private val lifecycleOwner: LifecycleOwner,
     private val context: Context,
-) : LifecycleObserver {
+) : LifecycleObserver, SharedPreferences.OnSharedPreferenceChangeListener {
+    private var receiverRegistered = false
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (!context.canWriteSecureSettings) {
@@ -64,24 +65,44 @@ class LockscreenShortcutHelper(
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun start() {
-        Timber.d("Registering receiver")
-        val filter = IntentFilter().apply {
-            addAction(Intent.ACTION_SCREEN_OFF)
-            addAction(Intent.ACTION_SCREEN_ON)
-            addAction(Intent.ACTION_USER_PRESENT)
-            priority = 999
-        }
-        context.registerReceiver(receiver, filter)
+        val localSettings = getPreferences(context)
+        val ready = localSettings.getString(LOCKSCREEN_LEFT_BUTTON, null) != null ||
+                localSettings.getString(LOCKSCREEN_RIGHT_BUTTON, null) != null
+        updateReceiverState(ready)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun stop() {
-        Timber.d("Unregistering receiver")
-        context.unregisterReceiver(receiver)
+        updateReceiverState(false)
+    }
+
+    private fun updateReceiverState(state: Boolean) {
+        if (state) {
+            if (!receiverRegistered) {
+                Timber.d("Registering receiver")
+                val filter = IntentFilter().apply {
+                    addAction(Intent.ACTION_SCREEN_OFF)
+                    addAction(Intent.ACTION_SCREEN_ON)
+                    addAction(Intent.ACTION_USER_PRESENT)
+                    priority = 999
+                }
+                context.registerReceiver(receiver, filter)
+                receiverRegistered = true
+            }
+        } else if (receiverRegistered) {
+            Timber.d("Unregistering receiver")
+            context.unregisterReceiver(receiver)
+            receiverRegistered = false
+        }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        start()
     }
 
     init {
         lifecycleOwner.lifecycle.addObserver(this)
+        getPreferences(context).registerOnSharedPreferenceChangeListener(this)
     }
 
     companion object {
