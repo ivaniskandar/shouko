d9ecbc3504571815448137ccae9a247024b2ae13
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-03-22 23:14:13 +0700

    PreferenceItems: Unify preference composables
    Inspired by AndroidX Preference's layouts

--- app/src/main/java/xyz/ivaniskandar/shouko/ui/PreferenceItems.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/ui/PreferenceItems.kt
@@ -3,8 +3,7 @@ package xyz.ivaniskandar.shouko.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,22 +11,38 @@ import androidx.compose.ui.unit.dp
 import xyz.ivaniskandar.shouko.ui.theme.ShoukoTheme
 
 @Composable
-fun BasePreference(
+private fun BasePreference(
+    modifier: Modifier = Modifier,
     title: String,
     subtitle: String? = null,
     enabled: Boolean = true,
+    widget: @Composable RowScope.() -> Unit = {}
 ) {
-    CompositionLocalProvider(
-        LocalContentAlpha provides if (enabled) ContentAlpha.high else ContentAlpha.disabled
+    Row(
+        modifier = modifier
+            .padding(horizontal = 14.dp, vertical = 16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = title, style = MaterialTheme.typography.subtitle1)
-    }
-    if (subtitle != null) {
-        CompositionLocalProvider(
-            LocalContentAlpha provides if (enabled) ContentAlpha.medium else ContentAlpha.disabled
-        ) {
-            Text(text = subtitle, modifier = Modifier.padding(top = 2.dp), style = MaterialTheme.typography.body2)
+        Column(modifier = Modifier.weight(1f)) {
+            CompositionLocalProvider(
+                LocalContentAlpha provides if (enabled) ContentAlpha.high else ContentAlpha.disabled
+            ) {
+                Text(text = title, style = MaterialTheme.typography.subtitle1)
+            }
+            if (subtitle != null) {
+                CompositionLocalProvider(
+                    LocalContentAlpha provides if (enabled) ContentAlpha.medium else ContentAlpha.disabled
+                ) {
+                    Text(
+                        text = subtitle,
+                        modifier = Modifier.padding(top = 2.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
         }
+        widget()
     }
 }
 
@@ -38,14 +53,12 @@ fun Preference(
     enabled: Boolean = true,
     onPreferenceClick: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .clickable(enabled = enabled, onClick = onPreferenceClick)
-            .padding(horizontal = 14.dp, vertical = 16.dp)
-            .fillMaxWidth()
-    ) {
-        BasePreference(title = title, subtitle = subtitle, enabled = enabled)
-    }
+    BasePreference(
+        modifier = Modifier.clickable(enabled = enabled, onClick = onPreferenceClick),
+        title = title,
+        subtitle = subtitle,
+        enabled = enabled
+    )
 }
 
 @Composable
@@ -56,33 +69,48 @@ fun SwitchPreference(
     enabled: Boolean = true,
     onCheckedChanged: (Boolean) -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .clickable(enabled = enabled) { onCheckedChanged.invoke(!checked) }
-            .padding(horizontal = 14.dp, vertical = 16.dp)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
+    BasePreference(
+        modifier = Modifier.clickable(enabled = enabled, onClick = { onCheckedChanged.invoke(!checked) }),
+        title = title,
+        subtitle = subtitle,
+        enabled = enabled
     ) {
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            BasePreference(title = title, subtitle = subtitle, enabled = enabled)
-        }
-        Spacer(modifier = Modifier.width(6.dp))
-        Switch(checked = checked, onCheckedChange = onCheckedChanged, enabled = enabled)
+        Switch(
+            checked = checked,
+            onCheckedChange = onCheckedChanged,
+            modifier = Modifier.padding(start = 6.dp),
+            enabled = enabled
+        )
     }
 }
 
 @Preview
 @Composable
 fun PreferenceItemsPreview() {
-    ShoukoTheme {
+    var darkTheme by remember { mutableStateOf(false) }
+    ShoukoTheme(darkTheme = darkTheme) {
         Surface(color = MaterialTheme.colors.background) {
             Column {
-                Preference(title = "Preference", subtitle = "With subtitle") {}
-                Preference(title = "Preference") {}
-                SwitchPreference(title = "Switch preference", subtitle = "With subtitle") {}
-                SwitchPreference(title = "Checked switch preference", checked = true) {}
+                var count by remember { mutableStateOf(0) }
+                Text(text = "Preference clicked $count time(s)")
+                Preference(title = "Preference", subtitle = "With subtitle") { count += 1 }
+                Preference(title = "Preference") { count += 1 }
+
+                var switch by remember { mutableStateOf(true) }
+                SwitchPreference(
+                    title = "Switch preference",
+                    subtitle = "With subtitle",
+                    checked = switch
+                ) {
+                    switch = !switch
+                }
+
+                SwitchPreference(
+                    title = "Dark theme",
+                    checked = darkTheme
+                ) {
+                    darkTheme = !darkTheme
+                }
             }
         }
     }
