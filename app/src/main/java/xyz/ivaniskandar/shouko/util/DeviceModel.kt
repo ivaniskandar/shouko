4190675fb332bd2dcba3083d16e210c812cebc47
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-04-05 13:58:02 +0700

    DeviceModel: Add new Xperia 5 II model number

--- app/src/main/java/xyz/ivaniskandar/shouko/util/DeviceModel.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/util/DeviceModel.kt
@@ -4,8 +4,23 @@ import android.os.Build
 
 object DeviceModel {
     // Xperia 5 II
-    val isPDX206 = arrayOf("A002SO", "SO-52A", "SOG02", "XQ-AS52", "XQ-AS62", "XQ-AS72").contains(Build.MODEL)
+    val isPDX206 = arrayOf(
+        "A002SO",
+        "SO-52A",
+        "SOG02",
+        "XQ-AS42",
+        "XQ-AS52",
+        "XQ-AS62",
+        "XQ-AS72"
+    ).contains(Build.MODEL)
 
     // Xperia 1 II
-    val isPDX203 = arrayOf("SO-51A", "SOG01", "XQ-AT42", "XQ-AT51", "XQ-AT52", "XQ-AT72").contains(Build.MODEL)
+    val isPDX203 = arrayOf(
+        "SO-51A",
+        "SOG01",
+        "XQ-AT42",
+        "XQ-AT51",
+        "XQ-AT52",
+        "XQ-AT72"
+    ).contains(Build.MODEL)
 }
