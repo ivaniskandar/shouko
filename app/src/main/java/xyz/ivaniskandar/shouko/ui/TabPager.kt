d9e60a5991c4728bd3cf65c9c00d2792727468ef
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-04-23 00:43:19 +0700

    TabPager: Use provided pagerTabIndicatorOffset

--- app/src/main/java/xyz/ivaniskandar/shouko/ui/TabPager.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/ui/TabPager.kt
@@ -3,14 +3,9 @@ package xyz.ivaniskandar.shouko.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
-import androidx.compose.material.TabPosition
 import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
@@ -18,13 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.lerp
 import com.google.accompanist.insets.navigationBarsPadding
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.launch
 import java.util.Locale
@@ -70,33 +62,4 @@ fun TabPager(
             content = { page -> content.invoke(this@Column, page) }
         )
     }
-}
-
-/**
- * From Accompanist's sample
- */
-@OptIn(ExperimentalPagerApi::class)
-fun Modifier.pagerTabIndicatorOffset(
-    pagerState: PagerState,
-    tabPositions: List<TabPosition>,
-): Modifier = composed {
-    val targetIndicatorOffset: Dp
-    val indicatorWidth: Dp
-
-    val currentTab = tabPositions[pagerState.currentPage]
-    val nextTab = tabPositions.getOrNull(pagerState.currentPage + 1)
-    if (nextTab != null) {
-        // If we have a next tab, lerp between the size and offset
-        targetIndicatorOffset = lerp(currentTab.left, nextTab.left, pagerState.currentPageOffset)
-        indicatorWidth = lerp(currentTab.width, nextTab.width, pagerState.currentPageOffset)
-    } else {
-        // Otherwise we just use the current tab/page
-        targetIndicatorOffset = currentTab.left
-        indicatorWidth = currentTab.width
-    }
-
-    fillMaxWidth()
-        .wrapContentSize(Alignment.BottomStart)
-        .offset(x = targetIndicatorOffset)
-        .width(indicatorWidth)
 }
