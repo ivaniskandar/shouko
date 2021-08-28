03ff383d6d0541266c6c1003e326193cc7922f62
Zixuan Wang <zxwang42@gmail.com>
2021-08-28 04:48:16 +0000

    Add Xperia 1 III models

--- app/src/main/java/xyz/ivaniskandar/shouko/feature/GAKeyOverrider.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/feature/GAKeyOverrider.kt
@@ -315,7 +315,7 @@ class GAKeyOverrider(
         private const val GA_KEY_DISABLED_GLOBAL_SETTING_KEY = "somc.game_enhancer_gab_key_disabled"
 
         // Only supports Xperia 5 II
-        val isSupported = DeviceModel.isPDX206 || DeviceModel.isPDX213
+        val isSupported = DeviceModel.isPDX206 || DeviceModel.isPDX213 || DeviceModel.isPDX215
     }
 }
 
