4fb7256edbe974cd8f6348162b1d40ae210e76ef
Ivan Iskandar <iiiiskandar14@gmail.com>
2022-03-09 15:47:38 +0700

    Add new app shortcut to clean link in clipboard

--- app/src/main/java/xyz/ivaniskandar/shouko/activity/LinkCleanerTargetActivity.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/activity/LinkCleanerTargetActivity.kt
@@ -1,27 +1,76 @@
 package xyz.ivaniskandar.shouko.activity
 
-import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.window.Dialog
+import androidx.core.content.getSystemService
+import logcat.LogPriority
+import logcat.logcat
 import xyz.ivaniskandar.shouko.R
+import xyz.ivaniskandar.shouko.ui.theme.ShoukoM3Theme
 import java.net.URLDecoder
 
-class LinkCleanerTargetActivity : Activity() {
+class LinkCleanerTargetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (intent.action == Intent.ACTION_SEND) {
-            val oldLink = intent.getStringExtra(Intent.EXTRA_TEXT)
-            if (oldLink != null) {
-                shareLink(cleanLink(oldLink))
+        when (val action = intent.action) {
+            Intent.ACTION_SEND -> {
+                val oldLink = intent.getStringExtra(Intent.EXTRA_TEXT)
+                if (oldLink != null) {
+                    val newLink = cleanLink(oldLink)
+                    if (newLink != null) {
+                        shareLink(newLink)
+                        Toast.makeText(this, R.string.link_cleaner_success, Toast.LENGTH_SHORT).show()
+                    }
+                }
+                finish()
+            }
+            ACTION_CLEAN_CLIPBOARD -> {
+                setContent {
+                    ShoukoM3Theme {
+                        Dialog(onDismissRequest = {}, content = {})
+                    }
+                    SideEffect {
+                        val cm = getSystemService<ClipboardManager>()
+                        if (cm == null) {
+                            finish()
+                            return@SideEffect
+                        }
+                        val oldLink = cm.primaryClip?.getItemAt(0)?.text
+                        if (oldLink.isValidUrl()) {
+                            val newLink = cleanLink(oldLink.toString())
+                            if (newLink != null) {
+                                cm.setPrimaryClip(ClipData.newPlainText("cleaned link", newLink))
+                                Toast.makeText(this, R.string.link_cleaner_success_clipboard, Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(this, R.string.link_cleaner_failed_clipboard, Toast.LENGTH_SHORT).show()
+                        }
+                        finish()
+                    }
+                }
+            }
+            else -> {
+                logcat(LogPriority.ERROR) { "Unknown action: $action" }
+                finish()
             }
         }
-        finish()
     }
 
-    private fun cleanLink(oldLink: String): String {
+    private fun CharSequence?.isValidUrl(): Boolean {
+        return this != null && Patterns.WEB_URL.matcher(this).matches()
+    }
+
+    private fun cleanLink(oldLink: String): String? {
         try {
             var oldUri = Uri.parse(oldLink)
             when (oldUri.host) {
@@ -42,12 +91,12 @@ class LinkCleanerTargetActivity : Activity() {
             if (q != null) {
                 newUri.appendQueryParameter("q", q)
             }
-            Toast.makeText(this, getString(R.string.link_cleaner_success), Toast.LENGTH_SHORT).show()
             return newUri.toString()
         } catch (e: Exception) {
+            logcat(LogPriority.ERROR) { e.stackTraceToString() }
             // Return oldLink
             Toast.makeText(this, getString(R.string.link_cleaner_failed), Toast.LENGTH_SHORT).show()
-            return oldLink
+            return null
         }
     }
 
@@ -63,5 +112,9 @@ class LinkCleanerTargetActivity : Activity() {
             putExtra(Intent.EXTRA_EXCLUDE_COMPONENTS, arrayOf(componentName))
         }
         startActivity(newIntent)
+    }
+
+    companion object {
+        private const val ACTION_CLEAN_CLIPBOARD = "shouko.action.CLEAN_CLIPBOARD"
     }
 }
