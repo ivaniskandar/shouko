ad078686ae97b5a3fe87cc87abbd4e1339a9d7d2
Ivan Iskandar <iiiiskandar14@gmail.com>
2022-03-28 17:56:40 +0700

    LinkTargetChooser: Workaround for bottom sheet max width

--- app/src/main/java/xyz/ivaniskandar/shouko/activity/LinkTargetChooserActivity.kt
+++ app/src/main/java/xyz/ivaniskandar/shouko/activity/LinkTargetChooserActivity.kt
@@ -9,18 +9,25 @@ import android.provider.Settings
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -38,12 +45,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.isSpecified
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
@@ -55,6 +68,7 @@ import xyz.ivaniskandar.shouko.ui.theme.ShoukoM3Theme
 import xyz.ivaniskandar.shouko.ui.theme.surfaceColorAtElevation
 import xyz.ivaniskandar.shouko.util.loadIcon
 import xyz.ivaniskandar.shouko.util.loadLabel
+import androidx.compose.material.MaterialTheme as M2Theme
 
 /**
  * "Browser" activity as a workaround for App Link changes in S.
@@ -129,64 +143,77 @@ fun AppLinkChooserSheet(
     val context = LocalContext.current
     val haptic = LocalHapticFeedback.current
 
-    ModalBottomSheetLayout(
-        sheetState = state,
-        sheetShape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
-        sheetElevation = 0.dp,
-        sheetBackgroundColor = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp),
-        sheetContentColor = MaterialTheme.colorScheme.onSurface,
-        sheetContent = {
-            Text(
-                text = stringResource(R.string.link_chooser_dialog_title),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp),
-                color = MaterialTheme.colorScheme.onSurface,
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleLarge
-            )
-            Divider(color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12F))
-            LazyColumn(
-                contentPadding = WindowInsets.navigationBars.only(WindowInsetsSides.Bottom).asPaddingValues()
-            ) {
-                items(resolverIntent) { item ->
-                    ListItem(
-                        modifier = Modifier
-                            .combinedClickable(
-                                onLongClick = {
-                                    scope.launch { state.hide() }
-                                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                                    onItemLongClick(item)
-                                },
-                                onClick = {
-                                    scope.launch { state.hide() }
-                                    onItemClick(item)
-                                }
-                            )
-                            .padding(vertical = 2.dp),
-                        icon = {
-                            Image(
-                                bitmap = shadowWrapper.run(item.loadIcon(context)!!)
-                                    .toBitmap()
-                                    .asImageBitmap(),
-                                contentDescription = null,
-                                modifier = Modifier.size(36.dp)
-                            )
-                        },
-                        text = {
-                            Text(
-                                text = item.loadLabel(context)!!,
-                                color = MaterialTheme.colorScheme.onSurface,
-                                style = MaterialTheme.typography.bodyLarge
-                            )
-                        }
-                    )
+    Box {
+        Scrim(
+            color = M2Theme.colors.onSurface.copy(alpha = 0.32f),
+            onDismiss = {
+                scope.launch { state.hide() }
+            },
+            visible = state.targetValue != ModalBottomSheetValue.Hidden
+        )
+        ModalBottomSheetLayout(
+            modifier = Modifier
+                .widthIn(max = 640.dp)
+                .align(Alignment.BottomCenter),
+            scrimColor = Color.Transparent,
+            sheetState = state,
+            sheetShape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+            sheetElevation = 0.dp,
+            sheetBackgroundColor = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp),
+            sheetContentColor = MaterialTheme.colorScheme.onSurface,
+            sheetContent = {
+                Text(
+                    text = stringResource(R.string.link_chooser_dialog_title),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Divider(color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12F))
+                LazyColumn(
+                    contentPadding = WindowInsets.navigationBars.only(WindowInsetsSides.Bottom).asPaddingValues()
+                ) {
+                    items(resolverIntent) { item ->
+                        ListItem(
+                            modifier = Modifier
+                                .combinedClickable(
+                                    onLongClick = {
+                                        scope.launch { state.hide() }
+                                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                                        onItemLongClick(item)
+                                    },
+                                    onClick = {
+                                        scope.launch { state.hide() }
+                                        onItemClick(item)
+                                    }
+                                )
+                                .padding(vertical = 2.dp),
+                            icon = {
+                                Image(
+                                    bitmap = shadowWrapper.run(item.loadIcon(context)!!)
+                                        .toBitmap()
+                                        .asImageBitmap(),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(36.dp)
+                                )
+                            },
+                            text = {
+                                Text(
+                                    text = item.loadLabel(context)!!,
+                                    color = MaterialTheme.colorScheme.onSurface,
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                        )
+                    }
                 }
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-        },
-    ) {
-        // Empty
+                Spacer(modifier = Modifier.height(16.dp))
+            },
+        ) {
+            // Empty
+        }
     }
     var opened by remember { mutableStateOf(false) }
     LaunchedEffect(key1 = state.currentValue) {
@@ -198,5 +225,36 @@ fun AppLinkChooserSheet(
     }
     BackHandler {
         scope.launch { state.hide() }
+    }
+}
+
+@Composable
+private fun Scrim(
+    color: Color,
+    onDismiss: () -> Unit,
+    visible: Boolean
+) {
+    if (color.isSpecified) {
+        val alpha by animateFloatAsState(
+            targetValue = if (visible) 1f else 0f,
+            animationSpec = TweenSpec()
+        )
+        val dismissModifier = if (visible) {
+            Modifier
+                .pointerInput(onDismiss) { detectTapGestures { onDismiss() } }
+                .semantics(mergeDescendants = true) {
+                    onClick { onDismiss(); true }
+                }
+        } else {
+            Modifier
+        }
+
+        Canvas(
+            Modifier
+                .fillMaxSize()
+                .then(dismissModifier)
+        ) {
+            drawRect(color = color, alpha = alpha)
+        }
     }
 }
